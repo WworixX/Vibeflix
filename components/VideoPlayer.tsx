5b94983fd8f4ae315fb98956123708bf7ef16f35
WworixX <hamon.noah14@gmail.com>
2026-05-14 17:59:18 +0200

    Player: retrait du sandbox (providers detectent et refusent de jouer)

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -156,7 +156,6 @@ function ExternalPlayer({
             allow="autoplay; encrypted-media; fullscreen; picture-in-picture"
             allowFullScreen
             referrerPolicy="origin"
-            sandbox="allow-scripts allow-same-origin allow-forms allow-presentation allow-pointer-lock allow-orientation-lock"
             onLoad={() => setLoading(false)}
             className="absolute inset-0 h-full w-full"
           />
